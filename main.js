95df12fbc3963928270f00caa97b84ebef034467
fureszb <115637991+fureszb@users.noreply.github.com>
2023-03-29 09:29:44 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
 import { ADATLISTA } from "./adat.js";
+const kulcsLista = Object.keys(ADATLISTA[0]);
 
 $(function () {
-    osszallit();
+  
+    osszallit(ADATLISTA);
     const ELEM = $(".table");
     let tartalom = osszeAllit();
     ELEM.append(tartalom);
 });
 
-function osszallit() {
-    let txt = "<table>";
-    for (let index = 0; index < 3; index++) {
-        txt += `<tr>`;
-        const obejct = ADATLISTA
-        for (let index = 0; index < ADATLISTA.length; index++) {
-            txt += `
-                    <td>${ADATLISTA[index]}</td>
-                   `;
-        }
-        txt += `</tr>`;
+function osszallit(lista){
+    let txt = "<div class= `table-responsive`>";
+    txt += "<table class=`table table-stripped table-boarded table-hover`>";
+    txt += "<thead class=`table-dark`><tr>";
+    for(const key in kulcsLista) {
+        txt += `<th id='${key}' > ${kulcsLista[key]}  ⇅<th/>`;        
     }
-    
-    txt = "</table>";
-    console.log(txt);
+    txt += "<th></th></tr></thead>";
+    for (let index = 0; index < lista.length; index++) {
+        txt += "<tr>";
+        const object = lista[index];
+        for (const key in object) {
+            const element = object[key];
+            if (key === "nev") {
+                txt += `<th> ${element}</th>`;                
+            }
+            else{
+                txt += `<td> ${element}</td>`;
+            }
+        }
+        txt += '<td><button class="gomb torol" id="t${index}">✖</button></td></tr>';
+    }
+    txt += "</table></div>";
     return txt;
 };
