00480d565c05dcf43a2db01da875f0efd3a09041
fureszb <115637991+fureszb@users.noreply.github.com>
2023-04-19 10:37:20 +0200

    Update torol.js

--- torol.js
+++ torol.js
@@ -1,8 +1,13 @@
+import { ADATLISTA } from "./adat.js";
 export function Delete()
 {
     $(document).on("click",'.deletetd',function(){
-
+        var index = $(this).parents('tr:first').index();
+        ADATLISTA.splice(index,1);
         $(this).parents('tr:first').remove();
+
+       
+        
     
     });
     
