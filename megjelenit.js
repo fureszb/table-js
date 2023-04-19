855fad0b4d01d41be257ed8ade55d7594a3f6c96
fureszb <115637991+fureszb@users.noreply.github.com>
2023-04-19 18:28:02 +0200

    Update megjelenit.js

--- megjelenit.js
+++ megjelenit.js
@@ -1,30 +1,32 @@
 import { kulcsLista } from "./adat.js";
+
 export function megjelenit(lista) {
-    console.log(lista);
-    var table = $("#tablazat");
-    let txt = "";
-    txt += "<table class='table table-bordered text-center'>";
-    txt += "<thead>";
-    for (const key in kulcsLista) {
-        txt += `<th data-column="${kulcsLista[key]}" data-order="desc" class='table-dark'> ${kulcsLista[key]}</th>`;
+  console.log(lista);
+
+  var table = $("#tablazat");
+  let txt = "";
+  txt += "<table class='table table-bordered text-center'>";
+  txt += "<thead>";
+  for (const key in kulcsLista) {
+    txt += `<th data-column="${kulcsLista[key]}" data-order="desc" class='table-dark'> ${kulcsLista[key]}</th>`;
+  }
+
+  txt += `<th class='table-dark'>Törlés</th>`;
+  txt += `<th class='table-dark'>Modositás</th>`;
+  txt += "</thead>";
+  txt += "<tbody id='elementBody'>";
+  for (let index = 0; index < lista.length; index++) {
+    txt += "<tr>";
+    for (let kulcs in lista[index]) {
+      txt += `<td>${lista[index][kulcs]}</td>`;
     }
+    txt += `<td  class="text-danger deletetd">✘</td>`;
+    txt += `<td  class="text-success"><button class="edit">modosit</button></td>`;
+    txt += "</tr>";
+  }
+  txt += "</tbody>";
+  txt += "</table>";
 
-    txt += `<th class='table-dark'>Törlés</th>`
-    txt += `<th class='table-dark'>Modositás</th>`
-    txt += "</thead>";
-    txt += "<tbody id='elementBody'>";
-    for (let index = 0; index < lista.length; index++) {
-
-        txt += "<tr>";
-        for (let kulcs in lista[index]) {
-            txt += `<td>${lista[index][kulcs]}</td>`;
-        }
-        txt += `<td  class="text-danger deletetd">✘</td>`;
-        txt += `<td  class="text-success edit">modosit</td>`;
-        txt += "</tr>";
-
-    }
-    txt += "</tbody>";
-    txt += "</table>";
-    table.html(txt);
+  table.html(txt);
 }
+
